48849fdfc76fe2fe262a56a46cdf016dcc48353d
slp600kun <slp600hiro@outlook.jp>
2022-01-17 11:49:00 +0900

    add wav file

---
+++ GridFS.py
@@ -0,0 +1,12 @@
+import pymongo
+import gridfs
+
+
+def main():
+    client = pymongo.MongoClient("mongodb+srv://m001-student:m001-mongodb-basics@sandbox.btwbl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    database = client.Audio
+    fs = gridfs.GridFS(database)
+    fileID = fs.put( open('static/css/audio/Levitation21.wav'))
+
+if __name__ == '__main__':
+    main()
